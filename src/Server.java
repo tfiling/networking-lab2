143351d3f96867272cf1f5c1b95024657b67d8d9
gal tfilin <galtfilin@gmail.com>
2016-12-31 23:37:27 +0200

    created server publisher which will publish the server over udp, implemented some server

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,48 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
 
 public class Server implements Runnable {
+	
+	private ServerSocket serverSocket;
+	private Socket socket;
+	private Logger logger;
+	private int port = 6000;
+	
+	public Server(Logger logger)
+	{
+		this.logger = logger;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		//TODO
 		
 	}
+		
+	private void createServerSocket()
+	{
+		while (port <= 7000)
+		{//try openning a TCP socket starting from port 6000 up to 7000 until you will find an available port
+			try {
+				this.serverSocket = new ServerSocket(port);
+				break;//found port and created a socket, stop trying other ports
+			} catch (IOException e) {
+				this.port++;//failed to open socket with the current port, try with the next one
+			}
+		}
+		if (port > 7000)
+		{
+			this.serverSocket = null;
+		}
+	}
 
+	public boolean isServerAvailable()
+	{
+		//TODO
+		return false;
+	}
 }
---
+++ src/ServerPublisher.java
@@ -0,0 +1,18 @@
+
+public class ServerPublisher implements Runnable {
+	
+	private Server server;
+	
+	public ServerPublisher(Server server)
+	{
+		this.server = server;
+	}
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
